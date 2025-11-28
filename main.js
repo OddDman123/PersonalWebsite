1d0189c39364d7655af1560fa857ea8811784239
OddDman123 <Kenjiww05@gmail.com>
2025-11-28 00:27:05 -0600

    Scrolling icons
    -Added behavior for scrolling icons that you can click on
    
    - added an iframe in the middle that changes depending on the button

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-const menu = document.querySelector('#days')
-// const menuLink = document.querySelector('.navbar__menu')
+const scrollContainer = document.getElementById('scroll');
+const mainFrame = document.getElementById('mainFrame');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('active')
-    print("clicked")
-    // menuLink.classList.toggle('active')
-})
+scrollContainer.addEventListener('wheel', (event) => {
+
+  event.preventDefault(); // Prevents default vertical page scrolling
+
+  // Adjust scroll amount based on wheel direction (deltaY)
+  scrollContainer.scrollBy({
+    left: event.deltaY < 0 ? -90 : 90, // Scroll left or right
+    behavior: 'smooth'
+  });
+});
+
+function change_page(index) {
+  if (!Number.isInteger(index))
+    return; //If the page index isn't an int return
+
+  switch (index) {
+    case 0:
+      mainFrame.src = 'https://github.com/OddDman123'
+      break;
+    default:
+      mainFrame.src = 'blog.html'
+  }
+}
