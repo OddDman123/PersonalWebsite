5f3bf1ec67d2e955c351c78dc01f1c2be982a2b8
OddDman <Kenjiww05@gmail.com>
2025-11-30 18:50:42 -0600

    new website update

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const scrollContainer = document.getElementById('scroll');
 const mainFrame = document.getElementById('mainFrame');
 
+const left_button = document.getElementById('left_button')
+const right_button = document.getElementById('right_button')
+
+// Event listners
 scrollContainer.addEventListener('wheel', (event) => {
 
   event.preventDefault(); // Prevents default vertical page scrolling
@@ -12,15 +16,36 @@ scrollContainer.addEventListener('wheel', (event) => {
   });
 });
 
+left_button.addEventListener('mouseover', function(event) {
+    scroll_icons(-1)
+});
+
+right_button.addEventListener('mouseover', function(event) {
+    scroll_icons(1)
+});
+
+//Functions
 function change_page(index) {
   if (!Number.isInteger(index))
     return; //If the page index isn't an int return
 
   switch (index) {
     case 0:
-      mainFrame.src = 'https://github.com/OddDman123'
+      mainFrame.src = 'AboutmePage/aboutme.html'
       break;
     default:
       mainFrame.src = 'blog.html'
   }
+}
+
+
+
+function scroll_icons(direction) {
+  if (!Number.isInteger(direction))
+    return; //If the page index isn't an int return
+
+  scrollContainer.scrollBy({
+    left: direction < 0 ? -90 : 90, // Scroll left or right
+    behavior: 'smooth'
+  });
 }
